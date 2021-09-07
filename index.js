9463ec18045b198fbe3ec32e9cf6b5e0b2001b1a
digallardox <digallardox@gmail.com>
2021-09-07 11:04:52 -0500

    done, I think LOL

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 #! /usr/bin/env node
-// ABOVE, allows us to run code globally as well as from the command line.
+
 const fs = require('fs')
 
-fs.readFile(__dirname + '/info.json', 'utf-8', function(err, data) {
+fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
     if (err) {
-        console.log(err);
+        console.log(err)
     } else {
-        console.log(data);
-        return data;
+        console.log(data)
+        return data
     }
-});
-
+})
